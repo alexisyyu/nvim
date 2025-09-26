657a4a307ea5033ceee3aa72b520dca61b3871a8
alexis <alexis.yyu@gmail.com>
2025-09-27 01:42:31 +0800

    add mini.comment

--- lua/config/mini_nvim.lua
+++ lua/config/mini_nvim.lua
@@ -1,1 +1,19 @@
-require("mini.surround").setup()
+require("mini.surround").setup(
+    {
+        mappings = {
+            add = '<leader>sa',
+            delete = '<leader>sd',
+            find = '<leader>sf',
+            find_left = '<leader>sF',
+            highlight = '<leader>sh',
+            replace = '<leader>sr',
+        }
+    }
+)
+require("mini.comment").setup({
+    mappings = {
+        comment = '<leader>/',
+        comment_line = '<leader>/',
+        comment_visual = '<leader>/',
+    }
+})
