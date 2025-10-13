06c47d188e06dcaf2cc1708aacce50be5fdc6f75
Yinuo <yu@YinuodeMacBook-Pro.local>
2025-10-13 16:50:20 +0800

    add tree sitter config

--- lua/config/tree_sitter.lua
+++ lua/config/tree_sitter.lua
@@ -1,3 +1,6 @@
 require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+	'html', 'css', 'javascript', 'typescript', 'tsx', 'markdown', 'sql'
+    },
     highlit = { enable = true }, 
 })
--- t.tsx
+++
@@ -1,7 +0,0 @@
-export default function t() {
-  return (
-    
-    <div></div>
-  )
-}
-
